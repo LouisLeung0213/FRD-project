1b6f7ec424eec05f197a19a117d2d93ea934a235
Louis Leung <louizleung@gmail.com>
2022-11-02 19:41:28 +0800

    50% of sign up MVC

--- back_end/src/users/users.controller.ts
+++ back_end/src/users/users.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  signUp(
+    @Body('username') username: string,
+    @Body('password') password: string,
+    @Body('nickname') nickname: string,
+    @Body('phone') phone: string,
+    @Body('email') email: string,
+    @Body('point') point: number,
+    @Body('is_admin') is_admin: boolean,
+  ): any {
+    return this.usersService.signUp(
+      username,
+      password_hash,
+      nickname,
+      phone,
+      email,
+      point,
+      is_admin,
+    );
+  }
+}
--- back_end/src/users/users.service.ts
+++ back_end/src/users/users.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './users.model';
+import { Knex } from 'knex';
+import { HTTPError } from 'error';
+import{}
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private knex: Knex) {}
+  //   users: User[] = [];
+
+  async signUp(
+    username: string,
+    password_hash: string,
+    nickname: string,
+    phone: string,
+    email: string,
+    point: number,
+    is_admin: boolean,
+  ): Promise<any> {
+    let checkUser = await this.knex
+      .select('username')
+      .from('users')
+      .where('username', username);
+    if (checkUser[0]) {
+      throw new HTTPError(401, 'username is taken');
+    }
+    let result = await this.knex("users")
+      .insert({
+        username,
+        password_hash: hashedPassWord,
+        phone,
+        email,
+        nickname,
+        point,
+        is_admin,
+      })
+      .returning("id");
+  }
+}
