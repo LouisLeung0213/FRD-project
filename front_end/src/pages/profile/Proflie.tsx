8b2f7d7ef9d528a14ce9b2f8f83334d841893bcb
Scott <tai199812@gmail.com>
2022-11-07 16:10:32 +0800

    profile in process

---
+++ front_end/src/pages/profile/Proflie.tsx
@@ -0,0 +1,22 @@
+import {
+  IonButton,
+  IonButtons,
+  IonCheckbox,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+
+// import "./Profile.css";
+
+const Profile: React.FC = () => {
+  return <IonContent></IonContent>;
+};
+
+export default Profile;
