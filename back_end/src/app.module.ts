367c8b96d0150096665e04b9d3345e2ad3da7865
Caleb <calebwongkaho1997@gmail.com>
2022-11-10 16:45:57 +0800

    first step of login backend

--- back_end/src/app.module.ts
+++ back_end/src/app.module.ts
@@ -2,17 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { KnexModule } from 'nestjs-knex';
 import { AuthModule } from './auth/auth.module';
-import * as config from '../knexfile';
 
 @Module({
   imports: [
-    KnexModule.forRootAsync({
-      useFactory: () => ({
-        config: config[process.env.NODE_ENV ?? 'development'],
-      }),
-    }),
     AuthModule,
     UsersModule,
   ],
--- back_end/src/users/users.module.ts
+++ back_end/src/users/users.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { KnexModule } from 'nestjs-knex';
+import * as config from '../../knexfile';
 
 @Module({
+  imports:[
+    KnexModule.forRootAsync({
+      useFactory: () => ({
+        config: config[process.env.NODE_ENV ?? 'development'],
+      }),
+    }),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService]
