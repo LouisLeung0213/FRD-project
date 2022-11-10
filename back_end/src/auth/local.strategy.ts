ff24d216b2d8e029fcddab9c3f7e1b66dee7df5f
Caleb <calebwongkaho1997@gmail.com>
2022-11-10 18:17:27 +0800

    finished login and signUp backend

--- back_end/src/auth/local.strategy.ts
+++ back_end/src/auth/local.strategy.ts
@@ -10,10 +10,12 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(username: string, password: string)/* 個form都要跟番username同password呢個叫法，改左就唔得 */: Promise<any> {
-    const user = await this.authService.validateUser(username, password);
-    if (!user) {
-      throw new UnauthorizedException();
+    try {
+      const user = await this.authService.validateUser(username, password);
+      return user;
+    } catch (error) {
+      return error
+        // throw new UnauthorizedException();
     }
-    return user;
   }
 }
