4a5350165a32dc7b9c0f2f857adcddf2888c9e93
Scott <tai199812@gmail.com>
2022-11-14 18:39:07 +0800

    firebase push noitification ios basic version done

--- front_end/ios/App/App/AppDelegate.swift
+++ front_end/ios/App/App/AppDelegate.swift
@@ -1,48 +1,15 @@
 import UIKit
-import FirebaseCore
 import Capacitor
 import Firebase
-import FirebaseInstanceID
-import FirebaseMessaging
-
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
-    let gcmMessageIDKey = "gcm.Message_ID"
 
     var window: UIWindow?
 
-    
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-        
-        // Push Notifications
-        
-        if #available(iOS 10.0, *) {
-          // For iOS 10 display notification (sent via APNS)
-          UNUserNotificationCenter.current().delegate = self
-
-          let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-          UNUserNotificationCenter.current().requestAuthorization(
-            options: authOptions,
-            completionHandler: { _, _ in }
-          )
-        } else {
-          let settings: UIUserNotificationSettings =
-            UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-          application.registerUserNotificationSettings(settings)
-        }
-
-        application.registerForRemoteNotifications()
-        
-        // Messaging Delegate
-        
-        Messaging.messaging().delegate = self
-        
-        
         return true
     }
 
@@ -80,101 +47,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // tracking app url opens, make sure to keep this call
         return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
     }
-    
-    
-// func application(_ application: UIApplication, _didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-//         Messaging.messaging().apnsToken = deviceToken
-//         InstanceID.instanceID().instanceID { (result, error) in
-//             if let error = error {
-//                 NotificationCenter.default.post(name: Notification.Name(CAPNotifications.DidFailToRegisterForRemoteNotificationsWithError.name()), object: error)
-//             } else if let result = result {
-//                 NotificationCenter.default.post(name: Notification.Name(CAPNotifications.DidRegisterForRemoteNotificationsWithDeviceToken.name()), object: result.token)
-//             }
-//         }
-//     }
+
+    // new Insert for Firebase push notifications
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-  NotificationCenter.default.post(name: .capacitorDidRegisterForRemoteNotifications, object: deviceToken)
-}
-
-func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-  NotificationCenter.default.post(name: .capacitorDidFailToRegisterForRemoteNotifications, object: error)
-}
-}
-
-// MARK: - Mark Section
-
-extension AppDelegate: UNUserNotificationCenterDelegate {
-    
-      // Receive displayed notifications for iOS 10 devices.
-      func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                  willPresent notification: UNNotification) async
-        -> UNNotificationPresentationOptions {
-        let userInfo = notification.request.content.userInfo
-
-        // With swizzling disabled you must let Messaging know about the message, for Analytics
-        // Messaging.messaging().appDidReceiveMessage(userInfo)
-
-        // ...
-
-        // Print full message.
-        print(userInfo)
-
-        // Change this to your preferred presentation option
-        return [[.alert, .sound]]
-      }
-
-      func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                  didReceive response: UNNotificationResponse) async {
-        let userInfo = response.notification.request.content.userInfo
-
-          
-        // ...
-
-        // With swizzling disabled you must let Messaging know about the message, for Analytics
-        // Messaging.messaging().appDidReceiveMessage(userInfo)
-
-        // Print full message.
-        print(userInfo)
-          
-          func application(_ application: UIApplication,
-                           didReceiveRemoteNotification userInfo: [AnyHashable: Any]) async
-            -> UIBackgroundFetchResult {
-            // If you are receiving a notification message while your app is in the background,
-            // this callback will not be fired till the user taps on the notification launching the application.
-            // TODO: Handle data of notification
-
-            // With swizzling disabled you must let Messaging know about the message, for Analytics
-            // Messaging.messaging().appDidReceiveMessage(userInfo)
-
-            // Print message ID.
-            if let messageID = userInfo[gcmMessageIDKey] {
-              print("Message ID: \(messageID)")
-            }
-
-            // Print full message.
-            print(userInfo)
-
-            return UIBackgroundFetchResult.newData
-          }
-
-      }
-    }
-
-
-
-
-extension AppDelegate: MessagingDelegate {
-    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-      print("Firebase registration token: \(String(describing: fcmToken))")
-
-      let dataDict: [String: String] = ["token": fcmToken ?? ""]
-      NotificationCenter.default.post(
-        name: Notification.Name("FCMToken"),
-        object: nil,
-        userInfo: dataDict
-      )
-      // TODO: If necessary send token to application server.
-      // Note: This callback is fired at each app startup and whenever a new token is generated.
-    }
+        Messaging.messaging().apnsToken = deviceToken
+        Messaging.messaging().token(completion: { (token, error) in
+        if let error = error {
+        NotificationCenter.default.post(name: .capacitorDidFailToRegisterForRemoteNotifications, object: error)
+        } else if let token = token {
+        NotificationCenter.default.post(name: .capacitorDidRegisterForRemoteNotifications, object: token)
+        }
+    })
+    }   
 
 }
--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -11,6 +11,7 @@ import {
 } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
 import { Redirect, Route, Switch } from "react-router-dom";
+
 import { PushNotifications } from "@capacitor/push-notifications";
 
 /* Core CSS required for Ionic components to work properly */
@@ -57,10 +58,45 @@ setupIonicReact();
 
 const App: React.FC = () => {
   useEffect(() => {
+    const registerNotifications = async () => {
+      let permStatus = await PushNotifications.checkPermissions();
+
+      if (permStatus.receive === "prompt") {
+        permStatus = await PushNotifications.requestPermissions();
+      }
+
+      if (permStatus.receive !== "granted") {
+        throw new Error("User denied permissions!");
+      }
+
+      await PushNotifications.register();
+    };
+
     const addListeners = async () => {
+      await registerNotifications();
+
       await PushNotifications.addListener("registration", (token) => {
         console.log("Registration token: ", token.value);
+        alert(JSON.stringify(token.value));
       });
+
+      await PushNotifications.addListener(
+        "pushNotificationReceived",
+        (notification) => {
+          console.log("Push notification received: ", notification);
+        }
+      );
+
+      await PushNotifications.addListener(
+        "pushNotificationActionPerformed",
+        (notification) => {
+          console.log(
+            "Push notification action performed",
+            notification.actionId,
+            notification.inputValue
+          );
+        }
+      );
     };
     addListeners();
   }, []);
