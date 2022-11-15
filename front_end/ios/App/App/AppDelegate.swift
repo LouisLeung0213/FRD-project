6da8b2b55eac46b70444d9e6d7e5aa7a5db9ad22
Scott <tai199812@gmail.com>
2022-11-15 16:09:50 +0800

    fixed ios package

--- front_end/ios/App/App/AppDelegate.swift
+++ front_end/ios/App/App/AppDelegate.swift
@@ -2,7 +2,6 @@ import UIKit
 import Capacitor
 import Firebase
 
-
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -48,15 +47,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // tracking app url opens, make sure to keep this call
         return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
     }
-
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         Messaging.messaging().apnsToken = deviceToken
         Messaging.messaging().token(completion: { (token, error) in
-        if let error = error {
-        NotificationCenter.default.post(name: .capacitorDidFailToRegisterForRemoteNotifications, object: error)
-        } else if let token = token {
-        NotificationCenter.default.post(name: .capacitorDidRegisterForRemoteNotifications, object: token)
-        }
-    })
-    }   
+            if let error = error {
+                NotificationCenter.default.post(name: .capacitorDidFailToRegisterForRemoteNotifications, object: error)
+            } else if let token = token {
+                NotificationCenter.default.post(name: .capacitorDidRegisterForRemoteNotifications, object: token)
+            }
+        })
+    }
 }
