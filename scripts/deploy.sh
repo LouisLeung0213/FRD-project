a1af2eb756794cf2378d0aa656b10e1fd1c05dad
Caleb <calebwongkaho1997@gmail.com>
2022-11-25 18:06:41 +0800

    local docker-compose up

---
+++ scripts/deploy.sh
@@ -0,0 +1,35 @@
+set -e
+set -x
+
+server="toystory"
+
+cd back_end
+./scripts/build-image.sh
+cd ..
+
+docker images web-server:latest
+
+## Export and Upload Image in one pass
+# docker save web-server:latest \
+#   | pv \
+#   | zstd \
+# 	| ssh $server "
+# 	  unzstd | docker load
+# 	"
+
+## Export image, then upload with rsync to allow skipping if the image is unchanged
+docker save web-server:latest \
+  | pv \
+  | zstd \
+  > image.zst
+rsync -SavLP image.zst $server:~/toystory/
+ssh $server "
+  cd toystory && \
+  cat image.zst | unzstd | docker load
+"
+
+rsync -SavLP docker-compose.yml $server:~/demo/
+ssh $server "
+  cd toystory && \
+  docker-compose up -d
+"
