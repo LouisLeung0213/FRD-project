cd68c729c62fa1d7dff94b34c858d97a3acfd38c
Louis Leung <louizleung@gmail.com>
2022-11-26 19:02:21 +0800

    merge

--- front_end/src/hooks/use-socket.tsx
+++ front_end/src/hooks/use-socket.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useMemo } from "react";
 import { connect } from "socket.io-client";
-import { WS_ORIGIN } from "../api";
+//import { WS_ORIGIN } from "../api";
 
+let WS_ORIGIN = "";
 export function useSocket() {
   const socket = useMemo(() => connect(WS_ORIGIN), []);
   useEffect(() => {
