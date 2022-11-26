d6647f1c5404a8ce72b3d3e5d342d40365a48126
Scott <tai199812@gmail.com>
2022-11-26 20:50:08 +0800

    merge

--- front_end/src/api.ts
+++ front_end/src/api.ts
@@ -1,3 +1,4 @@
-//export let API_ORIGIN = "http://localhost:1688";
-export let API_ORIGIN = "http://192.168.160.72:1688";
-export let FRONT_ORIGIN = "http://192.168.160.72:8100";
+export let API_ORIGIN = "http://localhost:1688";
+// export let API_ORIGIN = "http://192.168.160.72:1688";
+// export let FRONT_ORIGIN = "http://192.168.160.72:8100";
+export let FRONT_ORIGIN = "http://localhost:8100";
