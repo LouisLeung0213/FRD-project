752a7d0381a6e14f50109f72c55b594468d0353d
Caleb <calebwongkaho1997@gmail.com>
2022-12-01 16:49:56 +0800

    api

--- front_end/src/api.ts
+++ front_end/src/api.ts
@@ -1,10 +1,10 @@
-//export let API_ORIGIN = "http://localhost:1688";
+export let API_ORIGIN = "http://localhost:1688";
 
 //九龍灣
 //export let API_ORIGIN = "http://192.168.168.169:1688";
 
 //荃灣
-export let API_ORIGIN = "http://192.168.160.72:1688";
+// export let API_ORIGIN = "http://192.168.160.72:1688";
 export let FRONT_ORIGIN = "http://192.168.160.72:8100";
 // export let FRONT_ORIGIN = "http://localhost:8100";
 //export let FRONT_ORIGIN = "http://localhost:8100";
