42c745a6142441ae8ea65acc4a373d698cd3ac0e
Scott Tai <tai199812@gmail.com>
2022-12-07 23:15:28 +0800

    merging

--- front_end/src/api.ts
+++ front_end/src/api.ts
@@ -5,7 +5,7 @@
 //export let API_ORIGIN = "http://192.168.168.169:1688";
 
 //荃灣
-export let API_ORIGIN = "http://192.168.160.72:1688";
+export let API_ORIGIN = "http://192.168.160.167:1688";
 //export let FRONT_ORIGIN = "http://192.168.160.72:8100";
 export let FRONT_ORIGIN = "http://localhost:8100";
 //export let FRONT_ORIGIN = "http://localhost:8100";
